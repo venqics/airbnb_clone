cbfdd61356592e7804b31162a3e7ab25cbb50a45
venqics <venx.rao87@gmail.com>
2020-12-22 20:56:51 +0530

    made necessary migrations

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(core_models.TimeStampedModel):
+
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey()
+    room = models.ForeignKey()
