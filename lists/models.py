1b1842e2499505586e54e66950124e3b647f3aea
venqics <venx.rao87@gmail.com>
2020-12-23 12:25:33 +0530

    created models and admin customization for lists

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class List(core_models.TimeStampedModel):
+
+    """ List Model Definition """
+
+    name = models.CharField(max_length=80)
+    user = models.OneToOneField(
+        "users.User", related_name="list", on_delete=models.CASCADE
+    )
+    rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
+
+    def __str__(self):
+        return self.name
+
