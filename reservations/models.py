620881f04234729942d093dcc8d4dd3234d9a70f
venqics <venx.rao87@gmail.com>
2021-01-03 17:22:03 +0530

    changes done to reservations app

--- reservations/models.py
+++ reservations/models.py
@@ -1,8 +1,21 @@
+import datetime
 from django.db import models
-from core import models as core_models
 from django.utils import timezone
+from core import models as core_models
+
 # Create your models here.
 
+class BookedDay(core_models.TimeStampedModel):
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+    def __str__(self):
+        return str(self.day)
 class Reservation(core_models.TimeStampedModel):
 
     """ Reservation Model Definition """
@@ -42,6 +55,26 @@ class Reservation(core_models.TimeStampedModel):
     def is_finished(self):
         now = timezone.now().date()
         is_finished = now > self.check_out
+        if is_finished:
+            BookedDay.objects.filter(reservation=self).delete()
+        return is_finished
        
 
     is_finished.boolean = True
+
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            existing_booked_day = BookedDay.objects.filter(
+                day__range=(start, end)
+            ).exists()
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
+        return super().save(*args, **kwargs)
