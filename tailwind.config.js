8f42a8bbf7215ca6752f7aaedfe01124e0b4ad74
venqics <venx.rao87@gmail.com>
2021-01-03 21:46:21 +0530

    tailwind config and styles

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      },
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh"
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
