a59fa72d914141d0e7839c9581b853fc0f436b19
venqics <venx.rao87@gmail.com>
2021-01-07 21:50:15 +0530

    bucket_name

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "airbnb-clone-venk"
+AWS_STORAGE_BUCKET_NAME = "airbnb-clone-ven"
 AWS_AUTO_CREATE_BUCKET = True
 AWS_BUCKET_ACL = "public-read"
 
